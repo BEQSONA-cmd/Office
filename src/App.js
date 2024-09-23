3edadea3c707c7b9a9ba84a4b06f8c70e7ad9bc1
Beqa <tvildiani2001@gmail.com>
2024-09-23 20:55:43 +0200

    react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
+const conttainerStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh'
+};
+
+const boxStyle = {
+    padding: '1rem',
+    border: '1px solid #f4f4f4',
+    borderRadius: '5px',
+    backgroundColor: '#333'
+};
+
 const App = () => {
     return (
-        <div>
-            <h1>Welcome to My React App!</h1>
-            <p>This is a simple application using React.</p>
+        <div style={conttainerStyle}>
+            <div style={boxStyle}>
+                <h1>Welcome to My React App!</h1>
+                <p>This is a simple application using React.</p>
+            </div>
         </div>
     );
 };
