f14c3eabab23f9d3ab000dce95297260095d24f2
Beqa <tvildiani2001@gmail.com>
2024-09-24 10:53:00 +0200

    new modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',  // Entry point of your app
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
     },
-    mode: 'development',
     devServer: {
         static: {
-            directory: path.join(__dirname, 'public'), // Serve static files from the public folder
+            directory: path.join(__dirname, 'public'),
         },
-        port: 8080,  // Port where your server will run
-        open: true,  // Automatically open browser
-        hot: true,   // Enable hot module replacement
-    },
+        compress: true,
+        port: 8080,
+        hot: true,
+    mode: 'development',
     module: {
         rules: [
             {
-                test: /\.js$/,  // Transform JS files
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',  // Use Babel to transpile modern JS
+                    loader: 'babel-loader',
                 },
             },
         ],
