0f0d67099c2fbc02e44ad7cdd9dd5b57cb405d3f
Beqa <tvildiani2001@gmail.com>
2024-10-24 19:30:02 +0200

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,48 @@
-import Image from 'next/image';
+"use client";
 
-export default function Home() 
-{
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+export default function Home() {
     return (
         <article id="work" className="wrapper style3 py-16">
-          <div className="container mx-auto flex flex-wrap">
-            {/* Image on the left */}
-            <div className="w-full md:w-1/2 p-4">
-              <Image
-                className="large-image"
-                src="/images/me.jpg"
-                alt="Beqa Tvildiani"
-                width={400}
-                height={400}
-                style={{ borderRadius: '20%' }}
-              />
-            </div>
+            <div className="container mx-auto flex flex-wrap max-w-[900px]">
+                <motion.div
+                    initial={{ rotate: 0, scale:0 }}
+                    animate={{ rotate: 360, scale:1 }}
+                    transition={{ duration: 2 }}
+                    className="relative mx-auto !h-[300px] !w-[300px] md:w-1/2 p-4"
+                >
+                    <Image
+                        className="object-cover rounded-full !w-[300px] !h-[300px]"
+                        src="/images/me.jpg"
+                        alt="Beqa Tvildiani"
+                        fill={true}
+                    />
+                </motion.div>
 
-            {/* Text on the right */}
-            <div className="w-full md:w-1/2 p-4">
-              <header>
-                <h1 className="text-4xl font-bold">
-                  Hi. I'm <strong>Beqa Tvildiani</strong>.
-                </h1>
-              </header>
-              <p className="mb-4 text-lg font-medium">
-                <strong>I am a Software Engineer proficient 👨‍💻.</strong> Passionate about crafting efficient code, building dynamic web applications, and automating tasks.
-              </p>
-              <a href="portfolio">
-                  <button className="text-lg shadow__btn large scrolly bg-purple-600 hover:bg-purple-700 font-black py-3 px-6 rounded-lg hover:scale-105 duration-300">
-                  Get in touch with me
-              </button>
-              </a>
+                <div className="w-full md:w-1/2 p-4">
+                    <header>
+                        <h1 className="text-4xl mb-4 text-center font-bold">
+                            Hi. I'm <strong>Beqa Tvildiani</strong>.
+                        </h1>
+                    </header>
+                    <div className="text-center">
+                        <p className="mb-4 text-lg  font-medium">
+                            <strong>
+                                I am a Software Engineer proficient 👨‍💻.
+                            </strong>{" "}
+                            Passionate about crafting efficient code, building
+                            dynamic web applications, and automating tasks.
+                        </p>
+                        <a href="portfolio">
+                            <button className="text-lg shadow__btn large scrolly bg-purple-600 hover:bg-purple-700 font-black py-3 px-6 rounded-lg hover:scale-105 duration-300">
+                                Get in touch with me
+                            </button>
+                        </a>
+                    </div>
+                </div>
             </div>
-          </div>
         </article>
     );
-  }
-  
+}
