939189500b4f11458c9dcbe26217d9f7efe1f76e
Beqa <tvildiani2001@gmail.com>
2024-10-25 15:15:15 +0200

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import './globals.css';
 import Head from 'next/head';
 import React from 'react';
 
-
 export const metadata = {
   title: 'Beqa Tvildiani Portfolio',
   description: 'Welcome to Beqa Tvildiani\'s personal portfolio website',
@@ -11,14 +10,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      {/* <Head>
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      </Head> */}
       <body className="bg-white dark:bg-black text-gray-900 dark:text-gray-100">
         {/* Navbar */}
         <nav className="bg-gray-800 p-4">
