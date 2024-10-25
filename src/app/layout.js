9e31674104188187462a3c6d9ca412c578835b37
Beqa <tvildiani2001@gmail.com>
2024-10-25 16:31:51 +0200

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,14 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head> */}
       <body className="bg-white dark:bg-black text-gray-900 dark:text-gray-100">
         {/* Navbar */}
         <nav className="bg-gray-800 p-4">
